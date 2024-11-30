d48dc024baca699239e625a439f7894394a9ef4e
huylem1800vn <huylem1800vn@gmail.com>
2024-11-30 09:16:59 +0700

    Lấy ra danh sách công việc

---
+++ config/database.ts
@@ -0,0 +1,15 @@
+// const mongoose = require("mongoose");
+import mongoose from "mongoose";
+
+// module.exports.connect 
+export const connect = async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGO_URL}`);
+        console.log("Kết nối database thành công!");
+    } catch (error) {
+        console.log("Kết nối database thất bại!");
+        console.log(error);
+    }
+    
+}
+
