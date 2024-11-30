73f3e8e2293882d6f02b2f345368e2a63d3d0fe3
huylem1800vn <huylem1800vn@gmail.com>
2024-11-30 14:48:14 +0700

    Lấy ra chi tiết công việc

--- config/database.ts
+++ config/database.ts
@@ -2,7 +2,7 @@
 import mongoose from "mongoose";
 
 // module.exports.connect 
-export const connect = async () => {
+export const connect = async (): Promise<void> => {
     try {
         await mongoose.connect(`${process.env.MONGO_URL}`);
         console.log("Kết nối database thành công!");
